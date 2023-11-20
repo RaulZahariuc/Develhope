6feae2647214051d6157fee3e318284c4284092e
Miriana Covotta <raulzahariuc@gmail.com>
2023-11-20 12:14:40 +0100

    modified objcopy.js

--- esercizi/objcopy.js
+++ esercizi/objcopy.js
@@ -1,13 +1,13 @@
-const person1 = {        // qui vendono assegnate le proprietà all'oggetto "person1"
+const person1 = {        // qui vengono assegnate le proprietà all'oggetto "person1"
   firstName: "John",
   lastName: "Doe",
   age: 25,
 };
 
-const person2 = person1; // qui viene dichiarato che gli oggetti "person1" e "person2" hanno le stesse identiche proprietà 
+const person2 = person1; // qui viene dichiarato che gli oggetti "person1" e "person2" hanno le medesime proprietà 
 
-person2.firstName = "Simon"; // dato che i 2 oggetti hanno le stesse proprietà cambiando una di esse a "person2" la cambieremo di coseguenza anche a "person1",
-                             // inquanto, abbiamo dichiarato che entrambi gli oggetti hanno le stesse proprietà 
+person2.firstName = "Simon"; // poiché i 2 oggetti hanno le stesse proprietà, cambiando una di esse a "person2" modificheremo di coseguenza anche "person1"
+                             // in quanto abbiamo dichiarato che entrambi gli oggetti presentano le stesse proprietà 
 
 
 

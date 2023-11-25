79065320526c828b88f8186e74732cf7a026a1dc
Miriana Covotta <raulzahariuc@gmail.com>
2023-11-25 15:08:57 +0100

    new solved exercises

--- esercizi/somma.js
+++ esercizi/somma.js
@@ -1,5 +1,4 @@
 const num1 = 10;
 const num2 = 20;
-const sum = num1 + num2;
 
-console.log(`La somma di ${num1} e ${num2} è: ${sum}`);
+console.log(`La somma di ${num1} e ${num2} è: ${num1 + num2}`);

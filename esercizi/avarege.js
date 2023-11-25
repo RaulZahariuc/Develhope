d3410601181bed0227ffc849c605664900ac08ab
Miriana Covotta <raulzahariuc@gmail.com>
2023-11-25 14:19:39 +0100

    new solved exercises

--- esercizi/avarege.js
+++ esercizi/avarege.js
@@ -1,6 +1,6 @@
 function calculateAverageAge(persons) {
    
-    const totalAge = persons.reduce Math.reduce((sum, person) => sum + person.age, 0);
+    const totalAge = persons.reduce((sum, person) => sum + person.age, 0);
     const averageAge = totalAge / persons.length;
   
     return averageAge;
--- esercizi/rp1.js
+++ esercizi/rp1.js
@@ -2,9 +2,9 @@ function somma(...numeri) {
     return numeri.reduce((acc, numero) => acc + numero, 0);
   }
   
-  // Esempio di utilizzo della funzione
+  
   const risultato = somma(1, 2, 3, 4, 5);
   
-  // Stampa il risultato in console
+ 
   console.log("La somma è:", risultato);
   

1021a4ee3192037801ec5b8fe63bf163da722e3f
RaulZahariuc <raulzahariuc@gmail.com>
2024-01-21 16:49:20 +0100

    modified components exercises

--- esercizi/Components1/src/App.jsx
+++ esercizi/Components1/src/App.jsx
@@ -1,13 +1,16 @@
-// src/App.jsx
 import React from 'react';
 import Hello from './Hello';
+ /* 
+What happens when we use the Hello component multiple times: for each instance of the Hello component will be rendered in different instances.
+*/
 
-const App = () => {
+function App() {
   return (
     <div>
       <Hello />
     </div>
   );
-};
+}
 
 export default App;
+
--- esercizi/Components1/src/Hello.jsx
+++ esercizi/Components1/src/Hello.jsx
@@ -1,8 +1,20 @@
-// src/Hello.jsx
 import React from 'react';
+import Message from './Message';
+      /* 
+       Answer : The Message component will be rendered directly within the App component
+      */
 
-const Hello = () => {
-  return <h2>Hello, World!</h2>;
-};
+
+
+
+function Hello() {
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+      <Message />
+    </div>
+  );
+}
 
 export default Hello;
+
